42523d76082db4a13dd55865c8be35ad427f3d1d
Federico Builes <federico.builes@gmail.com>
2011-03-03 10:10:02 -0500

    Zipcodes bug.

--- charts.rb
+++ charts.rb
@@ -13,7 +13,7 @@ get '/groupon' do
   @total_coupons = GrouponDeal.hourly.num_coupons
   @total_spent = GrouponDeal.hourly.spent(:unique)
   @average_revenue = GrouponDeal.hourly.average_revenue(:unique)
-  @num_zip_codes = GrouponDeal.hourly.zip_codes.count
+  @num_zip_codes = GrouponDeal.hourly.zip_codes.length
 
   @closed_today = GrouponDeal.today.closed.length
   @coupons_today = GrouponDeal.num_coupons(:today)
