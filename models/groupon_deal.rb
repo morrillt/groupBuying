fab1c7a57035063d7719264971fbc3e767cb1b86
Federico Builes <federico.builes@gmail.com>
2011-03-04 14:42:37 -0500

    Cleaning up the house.

--- models/groupon_deal.rb
+++ models/groupon_deal.rb
@@ -5,21 +5,28 @@ class GrouponDeal < ActiveRecord::Base
 
   attr_reader :deals
 
-  scope :active, lambda { unique.where(:status   => true ) }
-  scope :closed, lambda { unique.where(:status   => false) }
-  scope :yesterday,  lambda { unique.where(:datadate => Date.today - 1.days) }
-  scope :today,  lambda { unique.where(:datadate => Date.today) }
-  scope :hourly, lambda { today.where("hour(now())=hour(time)") }
-  scope :unique, select("DISTINCT(deal_id), groupon.count, pricetext, datadate, location, status, urltext").order("time").group("deal_id")
-  scope :by_deal, lambda { |id| select("datadate, time, count, location, deal_id").where(:deal_id => id).where(:status => true).order("datadate DESC, time DESC") }
-  scope :by_day, lambda { |day| where(:datadate => day) }
+  scope :active,    lambda { unique.where( :status => true ) }
+
+  scope :closed,    lambda { unique.where( :status => false) }
+
+  scope :yesterday, lambda { unique.where( :datadate => Date.today - 1.days) }
+
+  scope :today,     lambda { unique.where( :datadate => Date.today) }
+
+  scope :hourly,    lambda { today.where("hour(now()) =hour(time)") }
+
+  scope :unique,    select("DISTINCT(deal_id), groupon.count, pricetext, datadate, location, status, urltext").order("time").group("deal_id")
+
+  scope :by_deal,   lambda { |id| select("datadate, time, count, location, deal_id").where( :deal_id => id).where( :status => true).order("datadate DESC, time DESC") }
+
+  scope :by_day,    lambda { |day| where( :datadate => day) }
 
   def self.num_coupons(range=:unique)
     self.send(range).map(&:count).inject(0) { |sum, n| sum += n.to_i }
   end
 
   def self.hot
-    GrouponDeal.find_by_sql("SELECT DISTINCT deal_id, hotindex, urltext, location FROM groupon WHERE status = '1' AND hour(time)=(hour(now())) ORDER BY hotindex DESC LIMIT 10;")
+    GrouponDeal.find_by_sql("SELECT DISTINCT deal_id, hotindex, urltext, location FROM groupon WHERE status = '1' AND hour(time)=hour(now()) ORDER BY hotindex DESC LIMIT 10;")
   end
 
   def self.spent(range=:unique)
@@ -43,32 +50,25 @@ class GrouponDeal < ActiveRecord::Base
     find_by_sql("SELECT deal_id, pricetext, time, count, datadate FROM groupon WHERE #{day_sql} AND status='1' AND hour(time)='#{hours}';")
   end
 
-  def hotness_index
-    @deals ||= GrouponDeal.by_deal(deal_id)
-    return 0 if @deals.empty?
-    present = @deals.first.count.to_f
-    past = @deals.last.count.to_f
-    return 0 if present == 0 or past == 0
-    GrouponDeal.change(past, present)
-  end
-
   def self.change(past, present)
     value = ((present.to_f - past.to_f) / past.to_f ) * 100
     GrouponDeal.round(value)
   end
 
-  # TODO DRY this up
   def self.chart_data
     daily_data = []
     aggregates = []
     yesterday_hours = []
+
     hours = Time.now.hour
+
+    # if we have less than 12 hours today we gotta take some hours from yesterday
     if hours < 12
       yesterday_hours = (0..23).to_a[-(12-hours)..-1]
     end
 
     yesterday_hours.each do |hour|
-      daily_data.push GrouponDeal.by_hour(hour, true)
+      daily_data.push GrouponDeal.by_hour(hour, :yesterday)
     end
 
     hours.times do |i|
@@ -78,7 +78,10 @@ class GrouponDeal < ActiveRecord::Base
 
     daily_data.each_with_index do |day, i|
       count = day.count
-      next if count == 0 || i == 0
+
+      # invalid data in the DB, just skip
+      next if count == 0
+
       average_price = day.map {|x| x.pricetext.to_f}.sum / count
 
       begin
@@ -89,7 +92,6 @@ class GrouponDeal < ActiveRecord::Base
       end
 
       num_deals = now_count - earlier_count
-
       aggregates << [day.first.time, self.round(average_price * num_deals.abs)]
     end
     [aggregates.map(&:first), aggregates.map(&:last)]
