8507af780d5c2e802b98355484b388591179cadc
Paul <paul@pogodan.com>
2011-03-10 21:59:14 +0100

    displaying deals

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,7 +1,13 @@
 class DealsController < InheritedResources::Base
+  belongs_to :site, :optional => true, :finder => :find_by_name
+  
   def import
     resource.import
     
     redirect_to resource, :notice => "Took new snapshot"
+  end
+  
+  def collection
+    @deals ||= end_of_association_chain.paginate(:page => params[:page])
   end
 end
