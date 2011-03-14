0e47e380e074f720e3d520c05c372c5d31169b58
Paul <paul@pogodan.com>
2011-03-14 14:25:04 +0100

    fix for chart on deal page

--- lib/chartable_controller.rb
+++ lib/chartable_controller.rb
@@ -6,7 +6,11 @@ module ChartableController
   end
   
   def load_chart
-    chartables = action_name == 'index' ? collection.first(6) : resource.deals.order(:revenue.desc).limit(6)
+    chartables = action_name == 'index' ? 
+      collection.first(6) : 
+      resource.respond_to?(:deals) ?
+        resource.deals.order(:revenue.desc).limit(6) :
+        resource
     
     @chart    = Chart.new(chartables, params.slice(:from, :to))
   end
