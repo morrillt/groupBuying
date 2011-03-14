aa03b47d2ba2d1c1beede9953d4e345c3d85697a
Paul <paul@pogodan.com>
2011-03-14 02:04:55 +0100

    merging changes from master

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'mongoid',    '2.0.0.rc7'
 gem 'bson_ext',   '1.2.4'
 gem 'mysql2'
 gem 'meta_where'
+gem 'daemons'
 gem 'exception_notification', :git => 'git://github.com/rails/exception_notification.git'
 
 # importer
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,66 +1,47 @@
-set :stages, %w(staging production)
-require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
+require 'rvm/capistrano'
+
+
+set :rvm_type, :user
+set :rvm_ruby_string, 'ree@charts'
 
 set :domain, 'group-buying.pogodan.com'
 set :application, domain
 
 default_run_options[:pty] = true
 set :repository,  "git@github.com:morrillt/groupBuying.git"
-set :branch, "schema_changes"
-#ssh_options[:keys] = ["~/.ec2/asanz.pem"]
 set :scm, "git"
 set :scm_username, "git"
 set :deploy_via, "remote_cache"
 set :git_enable_submodules, 1
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-set :deploy_to, "/var/www/vhosts/#{domain}"
 set :use_sudo, false
 
-ssh_options[:username] = 'capistrano'
-ssh_options[:forward_agent] = true
+ssh_options[:username] = 'root'
 
-#############################################################
-#	Passenger
-#############################################################
-
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-
-  desc "Restart the application"
-  task :restart do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
+task :staging do
+  server "50.56.83.165", :app, :web, :db, :primary => true
+  set :bundle, "bundle"
+  set :deploy_to, "/srv/gbd"
+  ssh_options[:username] = 'gbd'
 end
 
-#############################################################
-#       rake
-#############################################################  
-
-namespace :rake do  
-  # run like: cap staging rake:invoke task=a_certain_task
-  desc "Run a task on a remote server."  
-  task :invoke do  
-    run("cd #{deploy_to}/current; /usr/bin/env rake #{ENV['task']} RAILS_ENV=production")  
-  end  
-end
-
-#############################################################
-#	Shared stuff
-#############################################################
-task :after_update_code do
+after "deploy:update_code" do
   # trust rvmrc
   run "rvm rvmrc trust #{release_path}"
-  # 
-  # ['stylesheets/all.css', 'javascripts/all.js'].each do |cached_content|
-  #   run "rm -f #{release_path}/public/#{cached_content}"
-  # end
-  # 
-  # ['db/production.sqlite3'].each do |shared_file|
-  #   run "ln -nfs #{shared_path}/#{shared_file} #{release_path}/#{shared_file}"
-  # end
+
+  # link the default database.yml
+  run "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+
+#  run "cd #{deploy_to}/current && bundle install"
+end
+
+namespace :deploy do
+  task :start do
+    run "touch #{deploy_to}/current/tmp/restart.txt"
+  end
+
+  task :restart do
+    run "touch #{deploy_to}/current/tmp/restart.txt"
+  end
 end
