4913b20288106db3fb2d5f4dfdab296bbb461ed5
Tim Matheson <me@timmatheson.com>
2011-03-16 13:08:58 -0500

    updating deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repository,  "git@github.com:morrillt/groupBuying.git"
 set :branch, "groupie"
 set :scm, "git"
 set :scm_username, "git"
-set :deploy_via, "remote_cache"
+set :deploy_via, "checkout"
 set :git_enable_submodules, 1
 
 set :use_sudo, false
@@ -28,7 +28,7 @@ after "deploy:setup", "deploy:god:restart"
 after "deploy:update_code" do
 
   # link the default database.yml
-  run "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  run "ln -s #{shared_path}/config/database_groupie.yml #{release_path}/config/database_groupie.yml"
 end
 
 namespace :deploy do
