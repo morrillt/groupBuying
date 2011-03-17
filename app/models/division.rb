6e782d77b62e45f483eff2db10d61c62c9f14288
santiago <sgaviria@gmail.com>
2011-03-17 12:24:51 -0500

    fuck it

--- app/models/division.rb
+++
@@ -1,9 +0,0 @@
-class Division < ActiveRecord::Base
-  has_many :deals
-<<<<<<< HEAD
-  belongs_to :site
-=======
->>>>>>> 62992eb1545a85afc81867a39aecdb29e85392c0
-  
-  validates_uniqueness_of :name
-end
