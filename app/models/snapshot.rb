230b9b0a2ba178099b15dda0130441fc724d5dae
Tim Matheson <me@timmatheson.com>
2011-03-17 13:00:45 -0500

    adding output to snapshot for debugging

--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -6,8 +6,16 @@ class Snapshot < ActiveRecord::Base
   
   scope :recent, :conditions => ["created_at between ? and ?", 1.day.ago.at_midnight, Time.now], :include => [:site], :order => "created_at ASC"
   
+  def site_name
+    @site_name ||= self.site.name
+  end
+  
+  def deal_name
+    @deal_name ||= self.deal.name
+  end
+  
   def price
-    deal.try(:sale_price)
+    @price ||= deal.try(:sale_price)
   end
   
   def buyers_count
@@ -21,6 +29,7 @@ class Snapshot < ActiveRecord::Base
   private
   
   def capture_current_revenue
+    puts "Capturing snapshot for #{deal.inspect}"
     self.sold_count = deal.capture_snapshot
     self.sold_since_last_snapshot_count = (self.sold_count - deal.snapshots.last.sold_count)
   end
