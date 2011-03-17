f9a9b54018751877b9a9c1458072e8f5f630c921
santiago <sgaviria@gmail.com>
2011-03-17 12:37:12 -0500

    fixing

---
+++ app/controllers/divisions_controller.rb
@@ -0,0 +1,23 @@
+class DivisionsController < ApplicationController
+  # GET /divisions
+  # GET /divisions.xml
+  def index
+    @divisions = Division.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @divisions }
+    end
+  end
+
+  # GET /divisions/1
+  # GET /divisions/1.xml
+  def show
+    @division = Division.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @division }
+    end
+  end
+end
