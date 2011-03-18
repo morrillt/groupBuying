923b628b88e9fe5d1ccf80946f20829f2bde5a76
santiago <sgaviria@gmail.com>
2011-03-18 17:43:35 -0500

    added revenue methods to Chart

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -30,4 +30,49 @@ class Chart
       ]
     end
   end
+
+  # Returns a hash containing the revenue for each
+  # hour during the last 24 hours, for each site
+  def self.hourly_revenue_by_site
+    today= Time.now
+    chart= {
+      :categories => (1..24).map { |t| "#{(today-t.hours).hour}:00" }.reverse,
+      :series => []
+    }
+    Site.active.each do |site|
+      data= (1..24).map do |t|
+        today= Time.now-t.hours
+        site.revenue_by_given_hour_and_date(today.hour, today)
+      end.reverse
+      chart[:series] << { :name => site.name, :data => data }
+    end
+    chart
+  end
+
+  def self.hourly_revenue_by_divisions(site_id)
+    site= Site.find(site_id)
+    today= Time.now
+    chart= {
+      :categories => (1..24).map { |t| "#{(today-t.hours).hour}:00" }.reverse,
+      :series => []
+    }
+    pre_data= {}
+    divisions_names= []
+    (1..24).map do |t|
+      today= Time.now-t.hours # should not be Time.now
+      revenues= site.revenue_for_all_divisions_by_given_hour_and_date(today.hour, today)
+      revenues.each do |r|
+        if !pre_data[r.name]
+          divisions_names << r.name
+          pre_data[r.name]= {:revs=>[]}
+        end
+        pre_data[r.name][:revs] << r.rev
+      end
+    end
+    divisions_names.sort!
+    divisions_names.each do |d|
+      chart[:series] << { :name => d, :data => pre_data[d][:revs].reverse! }
+    end
+    chart
+  end
 end
