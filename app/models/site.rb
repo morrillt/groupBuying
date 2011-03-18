9d69467a449b15b8bae0c131bb9676585e65aa43
santiago <sgaviria@gmail.com>
2011-03-18 17:33:02 -0500

    added revenue methods

--- app/models/site.rb
+++ app/models/site.rb
@@ -33,4 +33,22 @@ class Site < ActiveRecord::Base
       raise Exception, "Unknown site source_name #{self.source_name}"
     end
   end
+
+  # Returns the total revenue for all active deals of this site
+  # for a given hour of a given date. 
+  def revenue_by_given_hour_and_date(hour, date)
+    snapshots= Snapshot.find_by_sql ["SELECT snapshots.deal_id, SUM(sold_since_last_snapshot_count) AS total_count, deals.sale_price FROM snapshots, deals WHERE snapshots.site_id = ? AND YEAR(snapshots.created_at) = ? AND MONTH(snapshots.created_at) = ? AND DAY(snapshots.created_at) = ? AND HOUR(snapshots.created_at) = ? AND sold_since_last_snapshot_count NOT IN (0) AND snapshots.deal_id = deals.id GROUP BY deal_id", self.id, date.year, date.month, date.day, hour]
+    
+    revenue= 0
+    snapshots.each do |s|
+      revenue += s.total_count * s.sale_price
+    end
+    revenue.to_f
+  end
+
+  # Returns the total revenue for each division of this site
+  # within the given hour of the given date
+  def revenue_for_all_divisions_by_given_hour_and_date(hour, date)
+    snapshots= Snapshot.find_by_sql ["SELECT deals.division_id AS division_id, divisions.name, SUM(total_count*price) AS rev FROM (SELECT snapshots.site_id, snapshots.deal_id, MAX(sold_since_last_snapshot_count) AS total_count, deals.sale_price AS price FROM snapshots, deals WHERE snapshots.site_id = ?	AND YEAR(snapshots.created_at) = ? AND MONTH(snapshots.created_at) = ? AND DAY(snapshots.created_at) = ? AND HOUR(snapshots.created_at) = ? AND sold_since_last_snapshot_count NOT IN (0) AND snapshots.deal_id = deals.id GROUP BY deal_id) revenue, deals, divisions WHERE revenue.deal_id = deals.id AND deals.division_id = divisions.id GROUP BY division_id", self.id, date.year, date.month, date.day, hour]
+  end
 end
