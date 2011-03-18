d896cd2787d3e2a07a5a17f8b0c06060efc9c1ed
Tim Matheson <me@timmatheson.com>
2011-03-18 12:12:32 -0500

    adding more specs for kgb deals

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,13 +1,13 @@
+Factory.define(:site) do |f|
+  f.source_name 'kgb_deals'
+end
+
 Factory.define(:deal) do |f|
   f.name "A great deal"
   f.permalink "http://someurl.com"
   f.actual_price 1.99
   f.sale_price 0.99
   f.division{ Factory(:division) }
-end
-
-Factory.define(:site) do |f|
-  f.source_name 'kgb_deals'
 end
 
 Factory.define(:division) do |f|
---
+++ spec/lib/snapshooter/lib/kgb_deals_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Snapshooter::KgbDeals do
+  before(:each) do
+    @kgb_deals = Snapshooter::KgbDeals.new
+  end
+  
+  context "a get request" do
+    it "should query the sitemap for divisions" do
+      @kgb_deals.should_receive(:get).with("/sitemap")
+      @kgb_deals.divisions
+    end
+  end
+  
+  context "crawl_new_deals" do
+    before(:each) do
+      @kgb_deals.stub(:divisions).and_return([{:href => 'http://google.com', :text => 'google'}])
+    end
+    
+    it "should get a division" do
+      @kgb_deals.should_receive(:get).with('http://google.com')
+      @kgb_deals.crawl_new_deals!
+    end
+    
+  end
+end
