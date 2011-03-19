ed6a156e143372884d19326736a861fe7dab4ce3
Tim Matheson <me@timmatheson.com>
2011-03-19 13:21:32 -0500

    adding chart spec

---
+++ spec/models/chart_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Chart do
+  before(:each) do
+    @site  = Factory(:site)
+  end
+   
+  context "calculations" do
+    it "should def calculate hourly_revenue_by_divisions" do
+      true
+    end
+  end 
+end
