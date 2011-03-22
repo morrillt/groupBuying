5a763d078acee691ab0af506a7ec305929dad42d
Tim Matheson <me@timmatheson.com>
2011-03-22 12:57:24 -0500

    adding csv exporter to external script and serving it up from disk

--- app/controllers/admin/deals_controller.rb
+++ app/controllers/admin/deals_controller.rb
@@ -11,29 +11,8 @@ class Admin::DealsController < Admin::ApplicationController
   end
   
   def export
-    @deals = Deal.active.find(:all, :include => [:division, :site], :conditions => ["created_at between ? and ?", 1.day.ago.at_midnight, Time.now])
     
-    FasterCSV.open('public/deals.csv','w+') do |csv|
-      csv << %w(id name url sale_price actual_price division site active hotness lat lng expires_at raw_address sold_count)
-      @deals.each do |deal|
-        csv << [
-          deal.id,
-          deal.name,
-          deal.permalink,
-          deal.sale_price,
-          deal.actual_price,
-          deal.division_name,
-          deal.site_name,
-          deal.active,
-          deal.hotness,
-          deal.lat,
-          deal.lng,
-          deal.expires_at,
-          deal.raw_address,
-          deal.buyers_count
-        ]
-      end
-    end
+    render_404 && return unless File.exists?("public/deals.csv")
     
     send_file("public/deals.csv", :content_type => "text/csv", :disposistion => "inline", :filename => "deals.csv")
   end
