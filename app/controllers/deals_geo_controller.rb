923c5a34f2ad1f7f31bdefb75d39b6731ea77278
santiago <sgaviria@gmail.com>
2011-03-23 17:46:15 -0500

    deals on GMaps

--- app/controllers/deals_geo_controller.rb
+++ app/controllers/deals_geo_controller.rb
@@ -2,5 +2,6 @@ class DealsGeoController < ApplicationController
   layout "dealsgeo"
 
   def index
+    @deals= Deal.joins(:site).select("deals.*, sites.source_name").where("lat IS NOT NULL AND lat NOT IN(0.0) AND lng IS NOT NULL AND lng NOT IN(0.0)").limit(25)
   end
 end
