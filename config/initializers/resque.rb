ed6ee1180d543aadf34c914d52c57df89a7ba45c
Tim Matheson <me@timmatheson.com>
2011-03-23 15:21:46 -0500

    Added resque and rescue scheduler. Configs, initializers, rake tasks

---
+++ config/initializers/resque.rb
@@ -0,0 +1,32 @@
+# Require app/jobs              
+Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
+                  
+# Redis config
+config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
+Resque.redis = Redis.new(:host => config['host'], :port => config['port'])
+              
+# Scheduler tasks
+require 'resque_scheduler'
+Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
+     
+# Hack for vegas
+module Vegas
+  class Runner         
+    
+    def port_open?(check_url = nil)
+      begin
+        check_url ||= url
+        options[:no_proxy] ? open(check_url, :proxy => nil) : open(check_url)
+        false
+      rescue Errno::ECONNREFUSED => e
+        true
+      rescue Errno::EPERM => e
+        # catches the "Operation not permitted" under Cygwin
+        true
+      rescue
+        true
+      end
+    end
+    
+  end
+end
