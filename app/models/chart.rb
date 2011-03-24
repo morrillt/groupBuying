d574f1be8dba700dcd6ec0b89fbdf6475fa7bba8
Tim Matheson <me@timmatheson.com>
2011-03-24 16:03:04 -0500

    removing unused code in chart.rb

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,36 +1,4 @@
 class Chart
-  # Accepts an array of deals or sites
-  def initialize(objects = [])
-    @objects = objects
-    # wrap non array classes in an array
-    @objects = [@objects].compact unless @objects.is_a?(Array)    
-    
-    if !@objects.empty?
-      # either site_id or deal_id
-      fk = "#{@objects.first.class.to_s.downcase}_id"
-      @snapshots = Snapshot.recent.find(:all, :conditions => ["#{fk} in(?)", @objects.map(&:id)])
-    else
-      @snapshots = Snapshot.recent
-    end
-    
-    # Group data by hour created
-    @snapshots_by_hour = @snapshots.group_by{ |s| s.created_at.strftime("%H") }
-  end
-  
-  def labels
-    @labels ||= @snapshots_by_hour.map{|h,s| h }.uniq!
-  end
-  
-  def datasets
-    @snapshots.group_by(&:site).map do |site, snapshots|
-      [site.name, 
-      snapshots.group_by{|s| s.created_at.strftime("%H") }.map do |h, snapshots|
-        snapshots.map(&:total_revenue)
-      end.flatten
-      ]
-    end
-  end
-
   # Returns a hash containing the revenue for each
   # hour during the last 24 hours, for each site
   def self.hourly_revenue_by_site
