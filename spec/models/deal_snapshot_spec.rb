0f5ef4d7a3fb0e696b64b7050bda819c3224affe
Tim Matheson <me@timmatheson.com>
2011-03-24 16:37:11 -0500

    updating the specs to reflect the new code

--- spec/models/deal_snapshot_spec.rb
+++ spec/models/deal_snapshot_spec.rb
@@ -4,10 +4,11 @@ describe DealSnapshot do
   before(:each) do
     @deal = Factory(:deal)
     @deal_snapshot = Factory(:deal_snapshot, :deal_id => @deal.id, :buyers_count => 100)
+    @deal.site.snapshooter.stub(:capture_deal).and_return(10)
   end
   
-  it "should populate values on save" do
-    @deal_snapshot.should_receive(:last_buyers_count=).with(100)
-    @deal_snapshot.save
+  it "should not create a snapshot for an expired deal" do
+    @deal.expires_at = 1.month.ago
+    DealSnapshot.create_from_deal!(@deal).should be_false
   end
 end
