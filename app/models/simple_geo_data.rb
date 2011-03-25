ca679b043eb990a1cdf5cbd4f3f2fa397178bcd9
Joan Ruiz <morden.curien@gmail.com>
2011-03-25 10:06:52 -0500

    Stoping simplegeo because we need more data

--- app/models/simple_geo_data.rb
+++ app/models/simple_geo_data.rb
@@ -1,29 +1,52 @@
 require 'lib/simple_geo_script/simple_geo'
-
-
 class SimpleGeoData < ActiveRecord::Base
-  def test_data
+
+  def test_ind_data deal
     sg =  SimpleGeo::SimpleGeoCollector.new
-    deals = Deal.find(:all, :select => 'raw_address', :limit => 100)
     init = Time.now
     fails = 0
     error = 0
     winning = 0
-    for d in deals do
+    begin
+      h = sg.parsing deal
+      r = sg.search h
+      if r.nil? 
+        puts "#{deal.id} fails"
+        fails += 1
+      else
+        puts "#{deal.id} wins"
+        winning += 1
+      end
+    rescue
+      puts "#{deal.id} died"
+      error +=1
+    end
+    puts "time: #{((Time.now)-init)/60} mins success:#{winning} fails:#{fails} error:#{error}"
+  end
+
+  def test_data
+    sg =  SimpleGeo::SimpleGeoCollector.new
+    deals = Deal.find(:all, :select => 'id,raw_address,telephone')
+    init = Time.now
+    fails = 0
+    error = 0
+    winning = 0
+    for deal in deals
       begin
-        h = sg.parsing d.raw_address
+        h = sg.parsing deal
         r = sg.search h
-        if r.nil?
+        if r.nil? 
+          puts "#{deal.id} fails"
           fails += 1
         else
-          winning +=1
+          puts "#{deal.id} wins"
+          winning += 1
         end
-        
       rescue
+        puts "#{deal.id} died"
         error +=1
       end
-    end    
-
-    puts "time:#{((Time.now)-init)/60} mins success:#{winning} fails:#{fails} error:#{error}"
+    end
+    puts "time: #{((Time.now)-init)/60} mins success:#{winning} fails:#{fails} error:#{error}"
   end
 end
--- lib/simple_geo_script/simple_geo.rb
+++ lib/simple_geo_script/simple_geo.rb
@@ -10,27 +10,41 @@ module SimpleGeo
       address = "#{hash[:address]} #{hash[:zip_code]}"
       options = {'radius' => 0.1}
       places = (SimpleGeo::Client.get_places_by_address(address,
-                                                        options))[:features]
-      for p in places
-        site = p if p[:properties][:phone].include?hash[:phone]
-      end
+                                                        options))
+      if places[:total]>0
+        features = places[:features]
+        for f in features
+          unless hash[:phone].nil? || hash[:address].blank?
+            site = f if f[:properties][:phone].include?hash[:phone]
+          end
+        end
+      else
+        return nil
+      end      
       site
     end
     
-    def parsing string
+    def parsing deal
       hash = { }
-      address = string.split ','
-      hash[:address] = address.first
-      temp = address.last.split ' '
-      hash[:state] = temp.first
-      hash[:zip_code] = temp.last.slice(0,5)
-      hash[:phone] = temp.last.slice(temp.last.length-4,temp.last.length).gsub(/[-]/,' ')
+      split = deal.raw_address.split ','
+      hash[:address] = split.first
+      hash[:state] = split.last
+      hash[:zip_code] = deal.telephone.slice(0,5)
+      hash[:phone] = deal.telephone.slice(deal.telephone.length-5,deal.telephone.length).gsub(/[-]/,' ')
       hash    
     end 
     
     def authenticate
       tokens = YAML::load(File.open("#{RAILS_ROOT}/config/simplegeo.yml"))
       SimpleGeo::Client.set_credentials(tokens['token'],tokens['secret_token'])
+    end
+
+    def get_data address
+      authenticate
+      options = {'radius' => 0.1}
+      places = (SimpleGeo::Client.get_places_by_address(address,
+                                                        options))
+      places
     end
   end
 end
