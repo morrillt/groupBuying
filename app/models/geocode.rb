9b1f3099cc19a9a2a0db793524fe03187ff37975
santiago <sgaviria@gmail.com>
2011-03-28 17:03:03 -0500

    geocoding

--- app/models/geocode.rb
+++ app/models/geocode.rb
@@ -4,12 +4,15 @@ class Geocode < ActiveRecord::Base
   belongs_to :deal
 
   def self.geocode
-    deals= Deal.where("created_at > ? AND active=1 AND lat NOT IN(0) AND lat IS NOT NULL AND lng NOT IN (0) AND lat IS NOT NULL", Time.now.advance(:days => -4)) \
-      .limit(250) \
-      .order("created_at DESC")
+    deals= Deal.where("created_at > ? AND active=1 AND lat NOT IN(0) AND lat IS NOT NULL AND lng NOT IN (0) AND lat IS NOT NULL", Time.now.advance(:days => -5)) \
+      .order("created_at DESC").group("raw_address")
     deals.each do |d|
-      result= MultiGeocoder.geocode(d.raw_address)
-      self.create(:lat => result.lat, :lng => result.lng, :formatted_address => result.full_address, :deal_id => id)
+      if (d.raw_address)
+        result= GoogleGeocoder.geocode(d.raw_address)
+        if result.lat && result.lng
+          self.create(:lat => result.lat, :lng => result.lng, :formatted_address => result.full_address, :deal_id => d.id)
+        end
+      end
     end
   end
 end
