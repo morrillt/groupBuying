60f81c1aaca7b71a243fdc7b04d1e5a68cdf9492
Tim Matheson <me@timmatheson.com>
2011-03-30 17:13:13 -0500

    adding new admin debug panel for sites

--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -1,11 +1,17 @@
 class Admin::SitesController < Admin::ApplicationController
-  layout "admin"
-  
   def index
-    @model_name= 'site'
+    @sites = Site.all
   end
-
+  
   def show
     @site = Site.find(params[:id])
+    @site_hourly_revenue_by_site = HourlyRevenueBySite.where(:site_id => params[:id]).limit(5)
+    
+    paginated_options = {:page => (params[:page] || 1), :per_page => 30}
+    if params[:query]
+      paginated_options.merge!(:conditions => ["deals.id = ? or deals.name like ?", params[:query], '%'+params[:query]+'%'])
+    end
+    
+    @deals = @site.deals.paginate(paginated_options)
   end
 end
