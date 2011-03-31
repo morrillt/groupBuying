72d0ced515abcbb9a41767bb83b83ac10bb3a564
Tim Matheson <me@timmatheson.com>
2011-03-30 19:35:42 -0500

    adding the deals.csv output dir to the public dir to simplify things

--- app/controllers/admin/deals_controller.rb
+++ app/controllers/admin/deals_controller.rb
@@ -10,7 +10,7 @@ class Admin::DealsController < Admin::ApplicationController
   end
   
   def export
-    csv_file_path = File.join(Rails.root, '..', 'shared', 'deals.csv')
+    csv_file_path = File.join(Rails.root, 'public', 'deals.csv')
     render_404 && return unless File.exists?(csv_file_path)
     send_file(csv_file_path, :content_type => "text/csv", :disposistion => "inline", :filename => "deals.csv")
   end
